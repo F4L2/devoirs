dca761647c58bd66cc525afa49a7384ef31dce2a
Alex <alex.ye@outlook.fr>
2020-02-15 18:06:17 +0100

    update

--- SPLEX/TME1/tme1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import scipy.stats as stats
-import matplotlib.pyplot as plt
-import statsmodels.sandbox.stats.multicomp as sm
-
-df_mice = pd.read_excel('data/Data_Cortex_Nuclear.xls')
-df_mice.fillna(inplace = True)
-print(df_mice.head())
